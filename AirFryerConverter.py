7fec71335c356a5819f304877c8c9c34ff6052d0
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-11-17 19:40:18 +0000

    Updated script

--- AirFryerConverter.py
+++ AirFryerConverter.py
@@ -1,6 +1,11 @@
+'''
+OVEN => AIR FRYER
+Temperature = Reduced by 20-30 degrees C
+Time = Reduced by 20%
+'''
 class Converter:
 
-    changeTemperature = -20
+    changeTemperature = 20
     changeTime = .20
 
     def __init__(self, appliance, temperature, time):
@@ -9,11 +14,25 @@ class Converter:
         self.time = time
 
     @classmethod
+    def toString(cls, temperature, time):
+        print("New temperature: {} C\nNew time: {} minutes".format(temperature, time))
+
+    @classmethod
     def convertToAirFryer(cls, temperature, time):
+        newTemp = temperature - Converter.changeTemperature
+        newTime = time - (time * Converter.changeTime)
+
+        Converter.toString(newTemp, newTime)
+    
+    @classmethod
+    def convertToOven(cls, temperature, time):
         newTemp = temperature + Converter.changeTemperature
-        newTime = time * Converter.changeTime
+        newTime = time + (time * Converter.changeTime)
 
-        print(newTemp, newTime)
+        Converter.toString(newTemp, newTime)
 
+oven = Converter('oven', 220, 15)
+Converter.convertToAirFryer(oven.temperature, oven.time)
 
-
+airfryer = Converter('airfryer', 220, 20)
+Converter.convertToOven(airfryer.temperature, airfryer.time)
